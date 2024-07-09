ee8c57f4935f5ad7b83aac1d11cee7fabf5437ff
subahan ali <subahanislam523@gmail.com>
2024-07-09 12:39:33 -0600

    project done deploy ment vercel

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173",
+    
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
